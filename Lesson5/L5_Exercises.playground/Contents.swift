683bbf8b1f8d3aa9e0fd24691eb050af36b0a6af
Bryan Bierce <bryanbierce@gmail.com>
2016-10-30 13:18:13 -0700

    Finish Lesson 5

--- Lesson5/L5_Exercises.playground/Contents.swift
+++ Lesson5/L5_Exercises.playground/Contents.swift
@@ -5,14 +5,24 @@ import UIKit
 //: __Problem 1.__
 //:
 //:Earlier we used the method, removeAtIndex() to remove the first letter of a string. This method belongs to the String class. See if you can use this same method to return the last letter of a string.
-
+func getLastLetterOf(_ word: String) -> Character {
+    var word = word
+    return word.remove(at: word.index(before: word.endIndex))
+}
 //:Test out your discovery below by returning the last letter of the String, "bologna".
 var word = "bologna"
-
+getLastLetterOf(word)
 //: __Problem 2__
 //:
 //: Write a function called combineLastCharacters. It should take in an array of strings, collect the last character of each string and combine those characters to make a new string to return. Use the strategy you discovered in Problem 1 along with a for-in loop to write combineLastCharacters. Then try it on the nonsenseArray below.
 var nonsenseArray = ["bungalow", "buffalo", "indigo", "although", "Ontario", "albino", "%$&#!"]
+func combineLastCharacters(collection: String, word: String) -> String {
+    var collection = collection
+    collection.append(getLastLetterOf(word))
+    return collection
+}
+let nonesenseWord = nonsenseArray.reduce("", combineLastCharacters)
+print(nonesenseWord)
 
 //: __Problem 3__
 //:
@@ -25,12 +35,25 @@ var nonsenseArray = ["bungalow", "buffalo", "indigo", "although", "Ontario", "al
 //: __3b.__ Write a for-in loop that checks each character of a string to see if it is a member of the "digits" set. Use the .unicodeScalars property to access all the characters in a string. Hint: the method longCharacterIsMember may come in handy.
 
 let digits = CharacterSet.decimalDigits
-
+func getDigitsIn(_ word: String) -> Bool {
+    for character in word.unicodeScalars {
+        if !digits.contains(UnicodeScalar(character.value)!) {
+            return false
+        }
+    }
+    return true
+}
 //: __Problem 4__
 //:
 //: Write a function that takes in an array of dirtyWord strings, removes all of the four-letter words, and returns a clean array.
 let dirtyWordsArray = ["phooey", "darn", "drat", "blurgh", "jupiters", "argh", "fudge"]
-
+func cleanWords(_ array: [String]) -> [String] {
+    func notFourLetters(_ word: String) -> Bool {
+        return word.characters.count != 4
+    }
+    return array.filter(notFourLetters)
+}
+print(cleanWords(dirtyWordsArray))
 //: __Problem 5__
 //:
 //: Write a method, filterByDirector, that belongs to the MovieArchive class.  This method should take in a dictionary of movie titles and a string representing the name of a director and return an array of movies created by that director. You can use the movie dictionary below. To test your method, instantiate an instance of the MovieArchive class and call filterByDirector from that instance.
@@ -38,6 +61,12 @@ let dirtyWordsArray = ["phooey", "darn", "drat", "blurgh", "jupiters", "argh", "
 var movies:Dictionary<String,String> = [ "Boyhood":"Richard Linklater","Inception":"Christopher Nolan", "The Hurt Locker":"Kathryn Bigelow", "Selma":"Ava Du Vernay", "Interstellar":"Christopher Nolan"]
 
 class MovieArchive {
-
+    func filterByDirector(movies: [String: String], director: String) -> [String] {
+        func hasDirector(collection: [String], movie: (name: String, director: String)) -> [String] {
+            return movie.director == director ? collection + [movie.name] : collection
+        }
+        return movies.reduce([], hasDirector)
+    }
 }
-
+let movieArchive = MovieArchive()
+print(movieArchive.filterByDirector(movies: movies, director: "Christopher Nolan"))
